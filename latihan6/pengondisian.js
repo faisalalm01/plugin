3542bb0a9ff6f1966f24de67dfd539a8249043ac
faisalalm01 <faisalali2858@gmail.com>
2021-01-27 22:54:15 +0700

    first commit

---
+++ latihan6/pengondisian.js
@@ -0,0 +1,38 @@
+//let plugin = 11;
+//if (plugin % 2 == 0){
+ //   console.log(plugin + " adalah bilangan genap ");
+//}else{
+  //  console.log(plugin + " adalah bilangan ganjil ");
+//}
+//for(let i=1; i <= 10; i++){
+ //   console.log(" hello world.. ");
+//}
+//let plugin = prompt(" input angka : ")
+//if (plugin % 2 == 0){
+//    alert(plugin + " adalah bilangan genap ");
+//}else{
+ //  alert(plugin + " adalah bilangan ganjil ");
+//}
+//let ulang = 1;
+//while(ulang <=30){
+ //   console.log("hello bro..");
+//    ulang++;
+//}
+//let angka =false;
+//do {
+//  console.log("HEllo Broo...");
+//}while(angka);
+
+//function plugin (){
+//  console.log("function biasa");
+//}
+
+//function ekspresi
+//let plugin = function(){
+//  console.log("function ekspresi");
+//}
+
+//function arrow
+//let plugin= () =>{
+//  console.log("function arrow");
+//}
---
+++ latihan6/tugas6/tugas6.js
@@ -0,0 +1,10 @@
+function deretGanjilGenap (angka){
+   let i ;
+   for (i = 1; i<=10 ; i++)
+   if(i % 2 == 1){
+      console.log(i + " bilangan ganjil");
+   }else{
+      console.log(i+ " bilangan genap");
+      
+   }
+}
